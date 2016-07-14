661a63110f89a820aabf5efdc3f4dd59ec9a0d23
temifowotade <temitope.fowotade@andela.com>
2016-07-14 14:35:57 +0100

    chore: styles other pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def friendly_date(d)
     d.strftime("%B %e, %Y")
   end
+
+  def class_name
+    if current_page?("/")
+      "homepage"
+    elsif current_page?("/dashboard")
+      "dashboard"
+    end
+  end
 end
